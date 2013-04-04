c1d6914c0ab83b5ab0e17a17c70162327598be22
Antonel Ernest Pazargic <antonel.pazargic@gmail.com>
2013-04-04 20:28:20 +0300

    Initial commit.

---
+++ src/ro/jtonic/handson/java8/MainClazz.java
@@ -0,0 +1,42 @@
+package ro.jtonic.handson.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * ***********************************************************************
+ * <p/>
+ * <b>Authors:</b>
+ * Antonel (Tony) Pazargic (antonel.pazargic@1and1.ro)
+ * <p/><b>Date:</b> 4/4/13
+ * <p/><b>Time:</b> 7:58 PM
+ * <p/>
+ * ***********************************************************************
+ */
+public class MainClazz {
+
+    public static void main(String[] args) {
+        schedule(() -> System.out.println("Hello world!!!"));
+
+        final List<String> versions = Arrays.asList("Java 7", "Java 8");
+
+        versions.sort(String::compareTo);
+        System.out.println("versions 1 = " + versions);
+
+        versions.sort((o1, o2) -> o2.compareTo(o1));
+        System.out.println("versions 2 = " + versions);
+
+
+        versions.filter(s -> s.equals("Java 8")).map(s -> String.format("Develop %s with pleasure",
+                s)).forEach(System.out::println);
+    }
+
+    private static void schedule(ITask task) {
+        task.run();
+    }
+
+    private static interface ITask {
+        public void run();
+    }
+
+}
