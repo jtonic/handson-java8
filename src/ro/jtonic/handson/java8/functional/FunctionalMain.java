7432d4594834602a3aa0012b7dfd185f6020deb2
jtonic <antonel.pazargic@gmail.com>
2014-03-27 12:17:44 +0200

    Add simple optional sample

--- src/ro/jtonic/handson/java8/functional/FunctionalMain.java
+++ src/ro/jtonic/handson/java8/functional/FunctionalMain.java
@@ -2,6 +2,7 @@ package ro.jtonic.handson.java8.functional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by jtonic on 10.03.2014.
@@ -15,6 +16,13 @@ public class FunctionalMain {
         final Integer sum1 = l.stream().reduce(0, Integer::sum);
         System.out.println("sum = " + sum);
         System.out.println("sum1 = " + sum1);
+
+        final String a1 = "A1";
+        final String a2 = null;
+        final String b1 = Optional.ofNullable(a1).orElse("A1_Default");
+        System.out.println("b1 = " + b1);
+        final String b2 = Optional.ofNullable(a2).orElse("A2_Default");
+        System.out.println("b2 = " + b2);
     }
 
 }
